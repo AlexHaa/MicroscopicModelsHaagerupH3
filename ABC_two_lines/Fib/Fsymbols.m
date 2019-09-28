66dd0cf4aef898372a82c52d730ab4ffc700176a
AlexHaa <53083946+AlexHaa@users.noreply.github.com>
2019-09-28 20:39:56 +0200

    Add files via upload

---
+++ ABC_two_lines/Fib/Fsymbols.m
@@ -0,0 +1,17 @@
+(* ::Package:: *)
+
+{F[1,1,1,1][1,1]->1,
+ F[1,\[Tau],\[Tau],1][\[Tau],1]->1,
+ F[1,1,\[Tau],\[Tau]][1,\[Tau]]->1,
+ F[1,\[Tau],1,\[Tau]][\[Tau],\[Tau]]->1,
+ F[1,\[Tau],\[Tau],\[Tau]][\[Tau],\[Tau]]->1,
+ F[\[Tau],1,1,\[Tau]][\[Tau],1]->1,
+ F[\[Tau],1,\[Tau],1][\[Tau],\[Tau]]->1,
+ F[\[Tau],\[Tau],1,1][1,\[Tau]]->1,
+ F[\[Tau],\[Tau],\[Tau],1][\[Tau],\[Tau]]->1,
+ F[\[Tau],1,\[Tau],\[Tau]][\[Tau],\[Tau]]->1,
+ F[\[Tau],\[Tau],1,\[Tau]][\[Tau],\[Tau]]->1,
+ F[\[Tau],\[Tau],\[Tau],\[Tau]][1,1]->2/(1+Sqrt[5]),
+ F[\[Tau],\[Tau],\[Tau],\[Tau]][1,\[Tau]]->Sqrt[2/(1+Sqrt[5])],
+ F[\[Tau],\[Tau],\[Tau],\[Tau]][\[Tau],1]->Sqrt[2/(1+Sqrt[5])],
+ F[\[Tau],\[Tau],\[Tau],\[Tau]][\[Tau],\[Tau]]->-2/(1+Sqrt[5])}
